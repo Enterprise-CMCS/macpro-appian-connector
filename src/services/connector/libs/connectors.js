ef9df9613532bad80a0225ef83bd414f0c959a55
James Dinh <jdinh@collabralink.com>
2023-01-24 16:39:32 -0800

    itterating

--- src/services/connector/libs/connectors.js
+++ src/services/connector/libs/connectors.js
@@ -11,7 +11,7 @@ export const connectors = [
       "poll.interval.ms": 2000,
       "batch.max.rows": 1000,
       mode: "timestamp+incrementing",
-      query: `SELECT CAST(PCKG_ID AS NUMERIC(38,0)) AS PCKG_ID, UPDT_TS FROM ${process.env.legacyschema}.MCP_SPA_PCKG`,
+      query: `SELECT CAST(PCKG_ID AS NUMERIC(38,0)) AS PCKG_ID, UPDT_TS, STATE_CD, RGN_CD, SPA_ID, CRNT_STUS, CRNT_STATE, PCKG_DSPSTN, PCKG_VRSN, PCKG_DRFT, PCKG_DAYS_ELPSD, PCKG_DAYS_ALLWD, SBMSSN_DATE, CREAT_USER_ID, CREAT_TS, UPDT_USER_ID, SRT_MLSTN_DATE, SRM_MLSTN_DATE FROM ${process.env.legacyschema}.MCP_SPA_PCKG`,
       "incrementing.column.name": "PCKG_ID",
       "timestamp.column.name": "UPDT_TS",
       "validate.non.null": false,
@@ -21,4 +21,3 @@ export const connectors = [
     },
   },
 ];
-
