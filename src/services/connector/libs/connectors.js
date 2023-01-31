bef269a7e9e26163d8e564e357ad612e2b9a8ecf
James Dinh <jdinh@collabralink.com>
2023-01-31 12:03:50 -0800

    cleanup

--- src/services/connector/libs/connectors.js
+++ src/services/connector/libs/connectors.js
@@ -15,16 +15,10 @@ export const connectors = [
       "incrementing.column.name": "PCKG_ID",
       "timestamp.column.name": "UPDT_TS",
       "validate.non.null": false,
-      "numeric.mapping": "best_fit",
       "key.converter": "org.apache.kafka.connect.json.JsonConverter",
       "key.converter.schemas.enable": false,
-      transforms: "createKey,extractInt,Cast",
-      "transforms.createKey.type":
-        "org.apache.kafka.connect.transforms.ValueToKey",
-      "transforms.createKey.fields": "PCKG_ID",
-      "transforms.extractInt.type":
-        "org.apache.kafka.connect.transforms.ExtractField$Key",
-      "transforms.extractInt.field": "PCKG_ID",
+      "numeric.mapping": "best_fit",
+      "transforms": "Cast",                
       "transforms.Cast.type": "org.apache.kafka.connect.transforms.Cast$Value",
       "transforms.Cast.spec":
         "PCKG_ID:int32,PCKG_VRSN:int32,PCKG_DAYS_ELPSD:int32,PCKG_DAYS_ALLWD:int32,HLC_ID:int32,SBMSSN_TYPE_PKG_ID:int32,LOCK_FLAG:int32,RVW_SQNC:int32,PRFL_ID:int32,HLC_LVL_ID:int32,SRC_DRAFT_PKG_ID:int32",
