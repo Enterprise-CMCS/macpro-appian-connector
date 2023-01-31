4bc530eea063c6a1fa3dbdcb6c095c6fb28bac32
James Dinh <jdinh@collabralink.com>
2023-01-31 09:01:38 -0800

    itteratting

--- src/services/connector/libs/query.js
+++ src/services/connector/libs/query.js
@@ -7,4 +7,4 @@ export const queryString = `SELECT CAST(ROUND(PCKG_ID) AS INTEGER) AS PCKG_ID, U
                     LOCK_FLAG, LOCK_BY, DLTD_FLAG, IS_CRNT_VRSN, IS_SBMTD, PRVNT_SUBMSN_FLAG, RVW_SQNC, 
                     RAI_FLAG, CLK_STUS, CNVRTD_FLAG, PEEK_FLAG, PRFL_ID, PRGRM_NAME, HLC_LVL_ID, ROUTG_INSTR, 
                     CLK_EXPRTN_FLAG, PRRTY_CD, SRC_DRAFT_PKG_ID, SUB_STUS
-                    FROM ${process.env.legacyschema}.MCP_SPA_PCKG`;
+                    FROM ${process.env.legacyschema}.MCP_SPA_PCKG;`;
