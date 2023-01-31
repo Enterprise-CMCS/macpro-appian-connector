74cd44841789ffbb1af145b53eb7516ac962f957
James Dinh <jdinh@collabralink.com>
2023-01-31 08:22:25 -0800

    trying dynamic property

--- src/services/connector/libs/connectors.js
+++ src/services/connector/libs/connectors.js
@@ -1,3 +1,5 @@
+import { queryString } from "./query";
+
 export const connectors = [
   {
     name: "source.jdbc.appian-dbo-1",
@@ -10,27 +12,15 @@ export const connectors = [
       "topic.prefix": `${process.env.topicNamespace}aws.appian.cdc.`,
       "poll.interval.ms": 2000,
       "batch.max.rows": 1000,
-      "table.whitelist": `${process.env.legacyschema}.MCP_SPA_PCKG`,
       mode: "timestamp+incrementing",
+      "table.whitelist": `${process.env.legacyschema}.MCP_SPA_PCKG`,
+      [`table.${process.env.legacyschema}.MCP_SPA_PCKG.query`]: queryString,
       "incrementing.column.name": "PCKG_ID",
       "timestamp.column.name": "UPDT_TS",
       "validate.non.null": false,
       "numeric.mapping": "best_fit",
       "key.converter": "org.apache.kafka.connect.json.JsonConverter",
       "key.converter.schemas.enable": false,
-
-      transforms: "createKey,extractInt,Cast",
-      "transforms.createKey.type":
-        "org.apache.kafka.connect.transforms.ValueToKey",
-      "transforms.createKey.fields": "PCKG_ID",
-      "transforms.extractInt.type":
-        "org.apache.kafka.connect.transforms.ExtractField$Value",
-      "transforms.extractInt.field": "PCKG_ID",
-      "transforms.Cast.type": "org.apache.kafka.connect.transforms.Cast$Value",
-      "transforms.Cast.spec":"PCKG_ID:int32",
     },
   },
 ];
-
-// "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-// "value.converter.schemas.enable": false,
