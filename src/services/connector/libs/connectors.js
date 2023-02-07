8ca57534af1eb21c9b75381bb5ff43b556791ca9
James Dinh <jdinh8124@gmail.com>
2023-02-07 10:00:09 -0800

    fix(topic name clarity): add table name to prefix (#43)
    * [OY2-22732] Security - Update OIDC service to have a variable subjectnamefilter (Part 2) (#33)
    
    * merge in changes from base-template
    
    * change repo name in serverless.yml
    
    * fix(Query Topic) Query data issue pointing at the wrong topic name(#38)
    
    * adjust connector
    
    * key to value
    
    * trying dynamic property
    
    * int
    
    * itterating
    
    * add round
    
    * itterating
    
    * itteratting
    
    * itteratting
    
    * using mmdl example again
    
    * cleanup
    
    * add millinds comments
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * add query
    
    * push query based on feedback
    
    * iterating
    
    * push query
    
    * itterating
    
    * cleanup
    
    * namechange
    
    * copy connector as is
    
    * cleanup
    
    * testjd
    
    * ADD ksqdb
    
    * bulk
    
    * cleanup
    
    * cleanup
    
    * iterate
    
    * adjusting
    
    * cleanup
    
    * iterate
    
    * remove bulk
    
    * add whitelist back
    
    * push no bulk
    
    * iteratting
    
    * cleanup
    
    * add query string
    
    * from cleanup
    
    * cleanup
    
    * cleanup connectors file
    
    * pre-commit files
    
    * fix(topic name clarity): add table name to prefix (#42)
    
    * adjust topic name changes (#46)
    
    * fix(topic name change): change topic name (#47)
    
    * fix(data issue fix): fix data topic dumping issue (#48)
    
    * change topic name
    
    * iteratting
    
    * iteratting
    
    * fix(deleteTopics): Give the deleteTopics kafkajs request more time to complete (#49)
    
    ---------
    
    Co-authored-by: JP Hechter <jhechter@fearless.tech>
    Co-authored-by: Mike Dial <48921055+mdial89f@users.noreply.github.com>

--- src/services/connector/libs/connectors.js
+++ src/services/connector/libs/connectors.js
@@ -2,14 +2,14 @@ import { queryString } from "./query";
 
 export const connectors = [
   {
-    name: "source.jdbc.appian-dbo-1",
+    name: "source.jdbc.appian-connector-dbo-1",
     config: {
       "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
       "tasks.max": 1,
       "connection.user": process.env.legacydbUser,
       "connection.password": process.env.legacydbPassword,
       "connection.url": `jdbc:oracle:thin:@${process.env.legacydbIp}:${process.env.legacydbPort}:${process.env.legacyDb}`,
-      "topic.prefix": `${process.env.topicNamespace}aws.appian.cdc`,
+      "topic.prefix": `${process.env.topicNamespace}aws.appian.cmcs.MCP_SPA_PCKG`,
       "poll.interval.ms": 2000,
       "batch.max.rows": 1000,
       query: queryString,
